ec6d9bbb8a6a9c01a763320239321c025f6f4ff5
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2026-03-20 21:17:10 -0300

    Added layout, navbar, footer and toogle theme for dark mode

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import { Gamepad2, Sun, Moon, Search } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const location = useLocation();
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+
+  return (
+    <nav className="sticky top-0 z-50 w-full backdrop-blur-sm bg-bg-surface/80 border-b border-border-default/20 shadow-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div>
+            <Link to="/" className="flex items-center gap-2 ">
+              <Gamepad2 className="w-8 h-8 text-accent-neon" />
+              <span className="font-bold text-xl bg-linear-to-r from-accent-primary to-accent-neon bg-clip-text text-transparent">
+                GamesAtlas
+              </span>
+            </Link>
+          </div>
+          <div className="flex items-center gap-6">
+            <Link
+              to="/about"
+              className={`text-sm font-medium transition-colors hover:text-accent-neon ${
+                location.pathname === "/about" ? "text-accent-neon" : ""
+              }`}
+            >
+              About Us
+            </Link>
+            <Link
+              to="/contact"
+              className={`text-sm font-medium transition-colors hover:text-accent-neon ${
+                location.pathname === "/contact" ? "text-accent-neon" : ""
+              }`}
+            >
+              Contact Us
+            </Link>
+            <Link
+              to="/browse"
+              className={`flex items-center gap-2 text-sm font-medium transition-colors hover:text-accent-neon ${
+                location.pathname === "/browse" ? "text-accent-neon" : ""
+              }`}
+            >
+              <Search className="w-4 h4" />
+              Browse
+            </Link>
+            <button
+              onClick={toggleTheme}
+              className="p-2 focus:outline-none rounded-full border border-border-default/50 hover:bg-bg-elevated hover:border-accent-primary/50 text-text-secondary hover:text-accent-neon transition-all cursor-pointer"
+            >
+              {theme === "dark" ? (
+                <Sun className="w-4 h-4" />
+              ) : (
+                <Moon className="w-4 h-4" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/PcGamesPage.jsx
+++
@@ -1,5 +0,0 @@
-const PcGamesPage = () => {
-  return <div>PcGamesPage</div>;
-};
-
-export default PcGamesPage;
